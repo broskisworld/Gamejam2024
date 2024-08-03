31269727641bbf486f550093bbc9273c4a82df18
Josh Bosley <broskisworld@gmail.com>
2024-08-03 09:57:00 -0600

    day 1

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
-
 @export var SPEED = 300.0
+@export var ROLL_SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
+@export var MIN_ROLL_SEC = 0.4
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = 0
 var was_on_floor = false
 var just_landed_on_floor = false
+var is_diving = false
+var is_rolling = false
+var roll_countdown = 0
+
+@onready var ap = $AnimationPlayer;
+@onready var sprite = $Sprite2D;
 
 func _physics_process(delta):
 	# Start of loop status vars
@@ -16,6 +23,10 @@ func _physics_process(delta):
 		just_landed_on_floor = true
 	else:
 		just_landed_on_floor = false
+	if is_rolling:
+		roll_countdown -= delta
+	if roll_countdown < 0:
+		is_rolling = false
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -24,18 +35,63 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	# Handle dives
+	if Input.is_action_pressed("move_down") and not is_on_floor():
+		is_diving = true
+	else:
+		is_diving = false
+	
+	# Are we starting a roll?
+	if is_on_floor() and velocity.x != 0 and Input.is_action_just_pressed("move_down"):
+		direction = 1 if velocity.x > 0 else -1
+		start_roll()
+	if just_landed_on_floor and Input.is_action_pressed("move_down") and (Input.get_axis("move_left", "move_right") != 0):
+		direction = Input.get_axis("move_left", "move_right")
+		start_roll()
 
 	# Get the input direction and handle the movement/deceleration.
-	if is_on_floor():
-		direction = Input.get_axis("move_left", "move_right")
-	elif Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
-		direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if is_rolling:
+		velocity.x = direction * ROLL_SPEED;
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			direction = Input.get_axis("move_left", "move_right")
+		elif Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
+			direction = Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	update_animation()
+	
+	# End of current physics loop status vars
+	was_on_floor = is_on_floor()
 
 	move_and_slide()
 	
-	# End of loop status vars
-	was_on_floor = is_on_floor()
+
+func start_roll():
+	if not is_rolling:
+		# Stay going in the same direction unless you're not moving
+		is_rolling = true
+		roll_countdown = MIN_ROLL_SEC
+
+func update_animation():
+	if false:
+		pass
+	elif is_rolling:
+		ap.play("roll")
+	elif velocity.y < 0:
+		ap.play("jump_up")
+	elif velocity.y > 0:
+		ap.play("jump_down")
+	elif velocity.x != 0:
+		ap.play("run")
+	else:
+		ap.play("idle")
+	
+	if Input.get_axis("move_left", "move_right") != 0:
+		sprite.flip_h = (Input.get_axis("move_left", "move_right") == -1)
+	
+	#is_rolling = ap.current_animation == "roll";
